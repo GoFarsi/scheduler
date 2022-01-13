dff51dfb8bccce5bc9b5f390c71b86a65d9d4830
Javad <ja7ad@live.com>
2022-01-13 21:27:25 +0330

    Add errors for scheduler

--- schedErrors/errors.go
+++ schedErrors/errors.go
@@ -1,11 +1,12 @@
-package error
+package schedErrors
 
 import "errors"
 
 var (
-	ERROR_TIME_FORMAT    = errors.New("error in time format")
+	ERROR_TIME_FORMAT    = errors.New("schedErrors in time format")
 	ERROR_PARAMETER      = errors.New("no parameters are adapted")
 	ERROR_NOT_A_FUNCTION = errors.New("in the job queue, only functions can be scheduled")
 	ERROR_JOB_PREIOD     = errors.New("no job period specified")
 	ERROR_NIL_REFLECTION = errors.New("reflection cannot be used with nil parameters")
+	ERROR_TRY_LOCK_JOB   = errors.New("with nil locker, trying to lock job")
 )
